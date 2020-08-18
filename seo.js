b37581e51c5ab520fbcaa60de111562e33b465c5
Vladimir Tarakanovskij <tarvl@bk.ru>
2020-08-18 14:47:19 +0300

    Парсинг и ключевые фразы

--- seo.js
+++ seo.js
@@ -15,32 +15,37 @@ document.querySelectorAll(".serp-item").forEach((item)=>{
 		yandex_serp.push(item.querySelector(serp_selector).innerText);
 	}
 });
-const yandex_serp_print=yandex_serp.map((ya_serp, i)=>`№${i+1+pagination_yandex_serp()*10} - ${ya_serp}`);
-console.log(yandex_serp_print.join(`\n`));
+const yandex_serp_with_pos=yandex_serp.map((ya_serp, i)=>`№${i+1+pagination_yandex_serp()*10} - ${ya_serp}`);
+console.log(`${yandex_serp.join(`\t`)}
+${yandex_serp_with_pos.join(`\n`)}`);
 
 /*2. Парсинг сайтов из органической выдачи из Гугла */
+clear();
 const google_serp=[];
 document.querySelectorAll(".g").forEach((item)=>{ 
 	let position_arrow = item.querySelector("cite").innerText.search(" ›");
 	google_serp.push(item.querySelector("cite").innerText.slice(0, position_arrow));
 });
-const google_serp_print=google_serp.map((go_serp, i)=>`№${i} - ${go_serp}`);
-console.log(google_serp_print.join(`\n`));
+const google_serp_with_pos=google_serp.map((go_serp, i)=>`№${i} - ${go_serp}`);
+console.log(`${google_serp.join(`\t`)}
+${google_serp_with_pos.join(`\n`)}`);
 
 /*3. Проверка нахождения элементов на странице */
 const nodes_site=['title', 'h1', 'strong', 'b', 'i', 'em'];
-nodes_site.map(node_length_text=>{let node_length_text_el = document.querySelectorAll(node_length_text);
-console.log(`Количество элементов ${node_length_text} = ${node_length_text_el.length}`);
-if(node_length_text_el.length>0){
-	node_length_text_el.forEach((item)=>{console.log(`Текст элеманта ${node_length_text}: ${item.innerText}`)})}
+nodes_site.map(node_text=>{let node_text_el = document.querySelectorAll(node_text);
+console.log(`Количество элементов ${node_text} = ${node_text_el.length}`);
+if(node_text_el.length>0){
+	node_text_el.forEach((item)=>{console.log(`Текст элемента ${node_text}: ${item.innerText}`)})}
 });
 
 /*4. Проверка на внешние ссылки */
+clear();
 const links_site=[];
 document.querySelectorAll("a").forEach((item)=>{links_site.push(item.href)});
 const nofollow_links=(site_address)=>{
 	const nofollow_link=links_site.filter(link_site=>link_site.search(site_address)==-1);
-	console.log(nofollow_link.join(`\n`));
+	if (nofollow_link.length>0) {console.log(nofollow_link.join(`\n`));} 
+	else {console.log('Внешние ссылки не найдены!');}
 }
 nofollow_links(window.location.hostname);
 
@@ -50,13 +55,14 @@ const input_rs=['robots.txt', 'sitemap.xml'];
 input_rs.map(async (rs)=>{
 	let response = await fetch(`/${rs}`);
 	let status_file = await response.ok;
-	let text_file = await response.text(); // прочитать тело ответа как текст
+	let text_file = await response.text();
 	if (status_file) {console.log(`Файл ${rs} есть в корне сайта! Его содержимое: 
 	${text_file}`);} 
 	else {console.log(`Файл ${rs} не найден!`);}
 });
 
 /*6. Проверка на наличие alt у изображений*/
+clear();
 const image_alt=[]; const image_no_alt=[];
 document.querySelectorAll("img").forEach((has_alt)=>{
 	if (has_alt.alt.search(/(.?[a-zA-Zа-яёА-ЯЁ]{3,}\s?.*)/gm)>-1) {image_alt.push(has_alt.alt);} 
